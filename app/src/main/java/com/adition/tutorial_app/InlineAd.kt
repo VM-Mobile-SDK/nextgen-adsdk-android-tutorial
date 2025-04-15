28d985b6f928a72d5dc34e015c25ba3aefa1ba4c
Robin Kaltenbach <robin.kaltenbach@adition.com>
2025-04-15 15:59:04 +0200

    Update ad event in inline ad.

--- app/src/main/java/com/adition/tutorial_app/InlineAd.kt
+++ app/src/main/java/com/adition/tutorial_app/InlineAd.kt
@@ -12,6 +12,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.adition.sdk_core.api.core.AdService
 import com.adition.sdk_core.api.core.Advertisement
 import com.adition.sdk_core.api.entities.request.AdRequest
+import com.adition.sdk_core.api.entities.response.AdMetadata
+import com.adition.sdk_core.api.services.event_listener.AdEventListener
+import com.adition.sdk_core.api.services.event_listener.AdEventType
 import com.adition.sdk_presentation_compose.api.Ad
 import kotlinx.coroutines.launch
 
@@ -39,15 +42,43 @@ class InlineAdViewModel: ViewModel() {
     var advertisementState = mutableStateOf<ResultState<Advertisement>?>(null)
     var aspectRatio = 2f
 
+    val adEventListener: AdEventListener = object : AdEventListener {
+        override fun eventProcessed(adEventType: AdEventType, adMetadata: AdMetadata) {
+            Log.d("InlineAdViewModel events", "Collected EVENT - $adEventType")
+            when (adEventType) {
+                is AdEventType.Impression -> {}
+                is AdEventType.RendererMessageReceived -> {}
+                is AdEventType.Tap -> {}
+                is AdEventType.UnloadRequest -> {}
+                is AdEventType.Viewable -> {
+                    when (adEventType.percentage) {
+                        AdEventType.VisibilityPercentage.ONE -> {
+                            Log.d("InlineAdViewModel events", "1% of my ads are now visible on the screen.")
+                        }
+                        AdEventType.VisibilityPercentage.FIFTY -> {
+                            Log.d("InlineAdViewModel events", "50% of my ads are now visible on the screen.")
+                        }
+                        AdEventType.VisibilityPercentage.ONE_HUNDRED -> {
+                            Log.d("InlineAdViewModel events", "100% of my ads are now visible on the screen.")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     init {
         viewModelScope.launch {
-            AdService.makeAdvertisement(adRequest).get(
+            AdService.makeAdvertisement(
+                adRequest,
+                adEventListener = adEventListener
+            ).get(
                 onSuccess = {
                     aspectRatio = it.adMetadata?.aspectRatio ?: aspectRatio
                     advertisementState.value = ResultState.Success(it)
                 },
                 onError = {
-                    Log.e("AdViewModel", "Failed makeAdvertisement: ${it.description}")
+                    Log.e("InlineAdViewModel", "Failed makeAdvertisement: ${it.description}")
                     advertisementState.value = ResultState.Error(it)
                 }
             )
