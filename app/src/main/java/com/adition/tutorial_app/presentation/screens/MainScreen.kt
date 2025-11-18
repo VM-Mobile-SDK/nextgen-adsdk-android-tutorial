8db13f8776dff2b39e574a67f6015405a0ad48a0
Artem Kvasnetskyi <154246288+kvasnetskyi-adition@users.noreply.github.com>
2025-11-18 18:01:30 +0200

    Global parameters tutorial updated using SDK 6.0.0

--- app/src/main/java/com/adition/tutorial_app/presentation/screens/MainScreen.kt
+++ app/src/main/java/com/adition/tutorial_app/presentation/screens/MainScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -17,6 +19,13 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.adition.ad_sdk.api.core.AdServiceProviderInterface
+import com.adition.ad_sdk.api.entities.exception.AdResult
+import com.adition.ad_sdk.api.entities.request.global_parameters.AccessMode
+import com.adition.ad_sdk.api.entities.request.global_parameters.AdRequestGlobalParameter
+import com.adition.ad_sdk.api.entities.request.global_parameters.AdRequestGlobalParameters
+import com.adition.ad_sdk.api.entities.request.global_parameters.ExternalUID
+import com.adition.ad_sdk.api.entities.request.global_parameters.GlobalParameter
+import com.adition.ad_sdk.api.entities.request.global_parameters.GlobalParameters
 import com.adition.tutorial_app.di.ServiceLocator
 import com.adition.tutorial_app.presentation.entities.PresentationState
 import com.adition.tutorial_app.presentation.screens.inline_screen.InlineRoute
@@ -35,6 +44,33 @@ fun MainScreen(
     viewModel: MainViewModel = viewModel()
 ) {
     val uiState by viewModel.state.collectAsState()
+    val showDialog by viewModel.showDialog.collectAsState()
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                viewModel.showDialog.value = false
+                viewModel.onLoad(false)
+            },
+            text = { Text("Please grant the permission to collect the data") },
+            confirmButton = {
+                TextButton(onClick = {
+                    viewModel.showDialog.value = false
+                    viewModel.onLoad(true)
+                }) {
+                    Text("Allow")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    viewModel.showDialog.value = false
+                    viewModel.onLoad(false)
+                }) {
+                    Text("Deny")
+                }
+            }
+        )
+    }
 
     PresentationStateContainer(
         uiState,
@@ -70,16 +106,81 @@ class MainViewModel(
     )
 
     val state = _state.asStateFlow()
+    val showDialog = MutableStateFlow(true)
 
-    init {
+    fun onLoad(isDataCollectionAllowed: Boolean) {
+        val globalParameters = GlobalParameters(
+            accessMode = isDataCollectionAllowed.toAccessMode()
+        )
+
+        val adRequestGlobalParameters = AdRequestGlobalParameters(
+            cookiesAccess = isDataCollectionAllowed.toCookiesAccess()
+        )
+
         viewModelScope.launch {
-            adServiceProvider.configure(
-                "1800",
-                parentCoroutineScope = this
-            ).get(
-                onSuccess = { _state.value = PresentationState.Loaded(Unit) },
-                onError = { _state.value = PresentationState.Error(it.description) }
-            )
+            adServiceProvider
+                .configure(
+                    "1800",
+                    parentCoroutineScope = this,
+                    globalParameters = globalParameters,
+                    adRequestGlobalParameters = adRequestGlobalParameters
+                )
+                .flatMap { adServiceProvider.get() }
+                .onSuccess { adService ->
+                    adService.setGlobalParameters(
+                        GlobalParameter(
+                            GlobalParameters::externalUID,
+                            ExternalUID("uid")
+                        )
+                    )
+
+                    adService.removeGlobalParameter(GlobalParameters::externalUID)
+                    adService.setAdRequestGlobalParameters(
+                        AdRequestGlobalParameter(
+                            AdRequestGlobalParameters::isIpIdentified,
+                            true
+                        )
+                    )
+
+                    adService.removeAdRequestGlobalParameter(
+                        AdRequestGlobalParameters::isIpIdentified
+                    )
+                }
+                .get(
+                    onSuccess = { _state.value = PresentationState.Loaded(Unit) },
+                    onError = { _state.value = PresentationState.Error(it.description) }
+                )
         }
+    }
+}
+
+private fun Boolean.toCookiesAccess(): AdRequestGlobalParameters.CookiesAccess {
+    return if (this) {
+        AdRequestGlobalParameters.CookiesAccess.GET
+    } else {
+        AdRequestGlobalParameters.CookiesAccess.NO_COOKIES
+    }
+}
+
+private fun Boolean.toAccessMode() = if (this) AccessMode.OPT_IN else AccessMode.OPT_OUT
+
+private suspend fun <T, ActionResult> AdResult<T>.flatMap(
+    action: suspend (T) -> AdResult<ActionResult>
+): AdResult<ActionResult> {
+    return when (this) {
+        is AdResult.Success -> action(this.result)
+        is AdResult.Error -> AdResult.Error(this.error)
+    }
+}
+
+private suspend fun <T> AdResult<T>.onSuccess(
+    action: suspend (T) -> Unit
+) : AdResult<T> {
+    return when (this) {
+        is AdResult.Success -> {
+            action(this.result)
+            this
+        }
+        is AdResult.Error -> this
     }
 }
