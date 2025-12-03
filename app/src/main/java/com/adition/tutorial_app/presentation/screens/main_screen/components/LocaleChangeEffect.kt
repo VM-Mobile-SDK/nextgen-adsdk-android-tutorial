49b076356e819598c6a3346df78ac02912255ac6
Artem Kvasnetskyi <154246288+kvasnetskyi-adition@users.noreply.github.com>
2025-12-03 12:49:53 +0200

    The `LocaleChangeEffect` fixed to prevent calling on application start.

--- app/src/main/java/com/adition/tutorial_app/presentation/screens/main_screen/components/LocaleChangeEffect.kt
+++ app/src/main/java/com/adition/tutorial_app/presentation/screens/main_screen/components/LocaleChangeEffect.kt
@@ -2,20 +2,22 @@ package com.adition.tutorial_app.presentation.screens.main_screen.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.platform.LocalConfiguration
 import java.util.Locale
 
 @Composable
 fun LocaleChangeEffect(onLocaleChanged: (Locale) -> Unit) {
-    val locale = rememberCurrentLocale()
-
-    LaunchedEffect(locale) {
-        onLocaleChanged(locale)
-    }
-}
-
-@Composable
-private fun rememberCurrentLocale(): Locale {
     val configuration = LocalConfiguration.current
-    return configuration.locales[0]
+    val locale = configuration.locales[0]
+    val localeTag = locale.toLanguageTag()
+    val previousLocaleTag = rememberSaveable { mutableStateOf<String?>(null) }
+
+    LaunchedEffect(localeTag) {
+        if (previousLocaleTag.value != null && previousLocaleTag.value != localeTag) {
+            onLocaleChanged(locale)
+        }
+        previousLocaleTag.value = localeTag
+    }
 }
